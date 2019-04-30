fb8bd15c427552ddda9b751eda11868ff8cbb8c1
xiangmu <1921290143@qq.com>
2019-04-30 22:43:52 +0800

    完成

---
+++ js/loin.js
@@ -0,0 +1,25 @@
+//$(function(){
+////	出现提示
+//	function showTips(info){
+//		$('.tips>p').html(info).stop(true).fadeIn(500).delay(1000).fadeOut(500);
+//	}
+////	console.log($('.tips>p'));
+//	var phone=$("#phone");
+//	var mima=$("#mima");
+////	console.log(phone);
+//	$("#button").on(function(){
+//		console.log("11111");
+//		if(phone.val()==""){
+//			showTips("请输入手机号");
+//			return;
+//		}
+//		if(!/^1\d{10}$/.test(phone.val())){
+//		showTips("请输入手机号");
+//		return;
+//		if(mima.val()==""){
+//			showTips("请输入6位密码");
+//			return;
+//		}
+//	}
+//	})
+//})
